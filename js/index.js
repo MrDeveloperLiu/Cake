d609c324414b57a7fb6fdd34d4dd27057dfe87a1
刘杨 <liuyang@edaijia-inc.cn>
2020-04-30 16:15:40 +0800

    modify

--- js/index.js
+++ js/index.js
@@ -9,10 +9,10 @@ document.addEventListener("readystatechange", function (e) {
 function onLoadComplete() {
     var url = 'config/config.json';
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-      if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+    xmlhttp.onload = function() {
+        if (xmlhttp.status == 200) {
             document.getElementById("agent").innerHTML = xmlhttp.responseText;
-      }
+        }
     }
     xmlhttp.open("GET", url, true);
     xmlhttp.send(null);
