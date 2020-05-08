ddbeec6959b8560ca5105bdb3495e62f8d272fe8
刘杨 <liuyang@edaijia-inc.cn>
2020-05-08 16:09:33 +0800

    jpg

--- config/product.js
+++ config/product.js
@@ -6,8 +6,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(6块)",
         "price" : 5800,
         "discount" : 100,
-        "img" : "img/quqi/IMG_1182.JPG",
-        "thumb" : "img/quqi/IMG_1182.JPG",
+        "img" : "img/src/IMG_1182.JPG",
+        "thumb" : "img/thumb/IMG_1182.JPG",
     },
     10012 : {
         "id" : 10012,
@@ -15,8 +15,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(6块)",
         "price" : 5000,
         "discount" : 100,
-        "img" : "img/quqi/IMG_1186.JPG",
-        "thumb" : "img/quqi/IMG_1186.JPG",
+        "img" : "img/src/IMG_1186.JPG",
+        "thumb" : "img/thumb/IMG_1186.JPG",
     },
     10013 : {
         "id" : 10013,
@@ -24,8 +24,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(6块)",
         "price" : 5800,
         "discount" : 100,
-        "img" : "img/quqi/IMG_1189.JPG",
-        "thumb" : "img/quqi/IMG_1189.JPG",
+        "img" : "img/src/IMG_1189.JPG",
+        "thumb" : "img/thumb/IMG_1189.JPG",
     },
     10014 : {
         "id" : 10014,
@@ -33,8 +33,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(6块)",
         "price" : 5000,
         "discount" : 100,
-        "img" : "img/quqi/IMG_1189.JPG",
-        "thumb" : "img/quqi/IMG_1189.JPG",
+        "img" : "img/src/IMG_1189.JPG",
+        "thumb" : "img/thumb/IMG_1189.JPG",
     },
     
     10021 : {
@@ -43,8 +43,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(500ML)",
         "price" : 2800,
         "discount" : 100,
-        "img" : "img/taro/IMG_1197.JPG",
-        "thumb" : "img/taro/IMG_1197.JPG",
+        "img" : "img/src/IMG_1197.JPG",
+        "thumb" : "img/thumb/IMG_1197.JPG",
     },
     
     10031 : {
@@ -53,8 +53,8 @@ var BDProductInfoDict = {
         "unit" : "一盒",
         "price" : 2200,
         "discount" : 100,
-        "img" : "img/cake/IMG_1184.JPG",
-        "thumb" : "img/cake/IMG_1184.JPG",
+        "img" : "img/src/IMG_1184.JPG",
+        "thumb" : "img/thumb/IMG_1184.JPG",
     },
     10032 : {
         "id" : 10032,
@@ -62,8 +62,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(3块)",
         "price" : 4500,
         "discount" : 100,
-        "img" : "img/cake/IMG_1198.JPG",
-        "thumb" : "img/cake/IMG_1198.JPG",
+        "img" : "img/src/IMG_1198.JPG",
+        "thumb" : "img/thumb/IMG_1198.JPG",
     },
     10033 : {
         "id" : 10032,
@@ -71,8 +71,8 @@ var BDProductInfoDict = {
         "unit" : "一个(6寸)",
         "price" : 17800,
         "discount" : 100,
-        "img" : "img/cake/IMG_1221.JPG",
-        "thumb" : "img/cake/IMG_1221.JPG",
+        "img" : "img/src/IMG_1221.JPG",
+        "thumb" : "img/thumb/IMG_1221.JPG",
         "tag" : "限定价",
     },
     
@@ -82,8 +82,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(360ML)",
         "price" : 4500,
         "discount" : 100,
-        "img" : "img/box_cake/IMG_1183.JPG",
-        "thumb" : "img/box_cake/IMG_1183.JPG",
+        "img" : "img/src/IMG_1183.JPG",
+        "thumb" : "img/thumb/IMG_1183.JPG",
     },
     10042 : {
         "id" : 10042,
@@ -91,8 +91,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(360ML)",
         "price" : 4000,
         "discount" : 100,
-        "img" : "img/box_cake/IMG_1185.JPG",
-        "thumb" : "img/box_cake/IMG_1185.JPG",
+        "img" : "img/src/IMG_1185.JPG",
+        "thumb" : "img/thumb/IMG_1185.JPG",
     },
     10043 : {
         "id" : 10043,
@@ -100,8 +100,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(360ML)",
         "price" : 4500,
         "discount" : 100,
-        "img" : "img/box_cake/IMG_1195.JPG",
-        "thumb" : "img/box_cake/IMG_1195.JPG",
+        "img" : "img/src/IMG_1195.JPG",
+        "thumb" : "img/thumb/IMG_1195.JPG",
     },
     10044 : {
         "id" : 10044,
@@ -109,8 +109,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(360ML)",
         "price" : 4000,
         "discount" : 100,
-        "img" : "img/box_cake/IMG_1187.JPG",
-        "thumb" : "img/box_cake/IMG_1187.JPG",
+        "img" : "img/src/IMG_1187.JPG",
+        "thumb" : "img/thumb/IMG_1187.JPG",
     },
     10045 : {
         "id" : 10045,
@@ -118,8 +118,8 @@ var BDProductInfoDict = {
         "unit" : "一个(4寸)",
         "price" : 2800,
         "discount" : 88,
-        "img" : "img/new/IMG_1244.JPG",
-        "thumb" : "img/new/IMG_1244.JPG",
+        "img" : "img/src/IMG_1244.JPG",
+        "thumb" : "img/thumb/IMG_1244.JPG",
         "tag" : "折扣价",
     },
 
@@ -129,8 +129,8 @@ var BDProductInfoDict = {
         "unit" : "一盒(4个)",
         "price" : 4800,
         "discount" : 88,
-        "img" : "img/new/IMG_1243.JPG",
-        "thumb" : "img/new/IMG_1243.JPG",
+        "img" : "img/src/IMG_1243.JPG",
+        "thumb" : "img/thumb/IMG_1243.JPG",
         "tag" : "折扣价",
     },
 
